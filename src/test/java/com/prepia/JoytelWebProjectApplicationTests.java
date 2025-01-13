fba16166a58dfdd3f437b6bbeee4ec82f21becac
E02 <chewjob99@gmail.com>
2025-01-13 14:44:55 +0900

    웹 프로젝트
    웹 프로젝트

---
+++ src/test/java/com/prepia/JoytelWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prepia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoytelWebProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
